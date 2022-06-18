f1bf5c89de7bdbbf142bdae3dccd12a16ef690ca
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-06-18 07:26:47 +0700

    Routes & View

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('/about', function () {
+    return view('about', [
+        "name" => "Rifky Alamsyah",
+        "email" => "rifkyalamsyah30@gmail.com",
+        "image" => "rifky-pic.png"
+    ]);
+});
+
+Route::get('/blog', function () {
+    return view('posts');
 });
