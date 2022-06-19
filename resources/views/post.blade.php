f04f7d0f4228e4eb2cafd1423d73fda90a5f49b7
Rifky Alamsyah <rifkyalamsyah30@gmail.com>
2022-06-19 16:38:12 +0700

    Post Category & Eloquent Relationship

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -5,6 +5,8 @@
 @section('container')
     <h2 class="mb-5">{{ $post->title }}</h2>
 
+    <p>By: Rifky Alamsyah in <a href="/categories/{{ $post->category->slug }}">{{ $post->category->name }}</a></p>
+
     {!! $post->body !!}
 
     <a href="/blog">Back to Posts</a>
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use App\Http\Controllers\PostController;
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+use App\Models\Category;
 
 /*
 |--------------------------------------------------------------------------
@@ -34,7 +35,21 @@ Route::get('/about', function () {
 
 
 Route::get('/blog', [PostController::class, 'index']);
-
-
 // Halaman single post
 Route::get('posts/{post:slug}', [PostController::class, 'show']);
+
+
+ROute::get('/categories', function(){
+    return view('categories', [
+        'title' => 'Post Categories',
+        'categories' => Category::all()
+    ]);
+});
+
+Route::get('/categories/{category:slug}', function (Category $category) {
+    return view('category', [
+        'title' => $category->name,
+        'posts' => $category->posts,
+        'category' => $category->name
+    ]);
+});
